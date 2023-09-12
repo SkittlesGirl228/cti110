665a044cc4782418b0c09421dbcc2f67a28d6340
SkittlesGirl228 <143529068+SkittlesGirl228@users.noreply.github.com>
2023-09-12 09:20:27 -0400

    Add files via upload
    First Lab submission

---
+++ P1Lab1_MoralesDelgadoJamarie.py
@@ -0,0 +1,48 @@
+# Checking if I have enough expenses covered for my trip
+# 09/07/2023
+# CTI-110 P1HW2 - Travel Expense
+# Your Name
+#
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+########### pseudocode ###########
+#
+
+# Information
+print("This tool will help calculate and displays travel expenses")
+budget = float(input("Enter Budget:  "))
+print()
+
+destination = input("Enter your travel destination:  ")
+print()
+
+gas = float(input("How much do you think you will spend on gas?  "))
+print()
+
+hotel = float(input("Approximately, how much will you need for accomodation/hotel:  "))
+print()
+
+food = float(input("Last, how much do you eed for food?  "))
+
+# Summary of Expenses
+expenses = gas + hotel + food
+balance = budget - expenses
+
+#Display results
+print("n--------Travel Expenses---------n")
+print("Location:", destination)
+print("Initial Budget:", budget)
+print("\nFuel:",gas)
+print("Accomodation:",hotel)
+print("Food:",food)
+print("\nRemaining Balance:",balance)
+
+print("\nTotal expenses:",expenses)
